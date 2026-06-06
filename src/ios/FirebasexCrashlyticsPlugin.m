e1ff8a064882c1f40e722c8f7d5bd06b0fd9e1a5
Dave Alden <dave.alden@srt-marine.com>
2026-06-06 10:05:35 +0100

    Fix cordova-ios@7 support using Cocoapods for Firebase SDK

--- src/ios/FirebasexCrashlyticsPlugin.m
+++ src/ios/FirebasexCrashlyticsPlugin.m
@@ -3,7 +3,13 @@
  * @brief iOS implementation of the FirebaseX Crashlytics Cordova plugin.
  */
 #import "FirebasexCrashlyticsPlugin.h"
-@import cordova_plugin_firebasex_core;
+#if __has_include("FirebasexCorePlugin.h")
+    // Cordova-ios 7 / CocoaPods: Files are compiled in a flat target structure
+    #import "FirebasexCorePlugin.h"
+#else
+    // Cordova-ios 8+ / SPM: Plugins are isolated Swift Package modules
+    @import cordova_plugin_firebasex_core;
+#endif
 @import FirebaseCrashlytics;
 
 /** Preference key for persisting the Crashlytics collection enabled state. */
